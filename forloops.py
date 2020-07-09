b162b0f9a47895119642f7817a4ccdfdd1f8edac
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-09 16:40:21 -0700

    forloops

---
+++ forloops.py
@@ -0,0 +1,9 @@
+for letter in "Denny Hong":
+    print(letter)
+
+names = ['Denny', "Amy", "Sam"]
+for i in range(len(names)):
+    if names[i] == "Amy":
+        # break
+        continue
+    print(names[i])
